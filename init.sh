c4147f4bdfbe33d1f4c61ace5e2becc2bc869605
phauer <info@philipphauer.de>
2016-09-18 21:10:53 +0200

    don't hard-code PYTHONPATH

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 source venv/bin/activate
 echo "Activated venv"
-#MY_PATH="`dirname \"$0\"`"
-#MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
-export PYTHONPATH="/home/pha/development/migrate-itunes-to-rhythmbox/src/main/python"
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+export PYTHONPATH="$SCRIPT_DIR/src/main/python"
 echo "Exported PYTHONPATH=$PYTHONPATH"
