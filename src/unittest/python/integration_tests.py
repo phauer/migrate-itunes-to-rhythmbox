57edc399e386dead8d0f6d8e39fa296f873d19b4
phauer <info@philipphauer.de>
2016-09-18 10:58:28 +0200

    close files

--- src/unittest/python/integration_tests.py
+++ src/unittest/python/integration_tests.py
@@ -17,8 +17,10 @@ class IntegrationTest(unittest.TestCase):
                                          source_library_root="D:/Music/",
                                          target_library_root="/home/pha/Music/")
 
-        actual_playlist_xml = target_path.open("r").read()
-        expected_playlist_xml = Path("../resources/expected_output/rhythmbox-playlists-modified.xml").open("r").read()
+        with target_path.open(mode="r", encoding="UTF-8") as target_path, \
+                Path("../resources/expected_output/rhythmbox-playlists-modified.xml").open("r") as expected_playlist_xml:
+            actual_playlist_xml = target_path.read()
+            expected_playlist_xml = expected_playlist_xml.read()
         self.assertEqual(actual_playlist_xml, expected_playlist_xml)
 
     def test_exclude_playlist_folders(self):
@@ -29,7 +31,8 @@ class IntegrationTest(unittest.TestCase):
                                          source_library_root="D:/Music/",
                                          target_library_root="/home/pha/Music/",
                                          exclude_playlist_folders=True)
-        actual_playlist_xml = target_path.open(mode="r", encoding="UTF-8").read()
+        with target_path.open(mode="r", encoding="UTF-8") as target_path:
+            actual_playlist_xml = target_path.read()
 
         self.assertTrue("name=\"PlaylistFolder\"" not in actual_playlist_xml, "Playlist folder should be excluded!")
         self.assertTrue("name=\"PlaylistFolder-PlaylistInFolder\"" in actual_playlist_xml, "Playlist of folder should be prefixed with folder name!")
