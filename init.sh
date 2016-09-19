8e986c9ceefe5636346aa8b3729b343d0d0292f6
phauer <info@philipphauer.de>
2016-09-19 17:10:07 +0200

    improve init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-source venv/bin/activate
-echo "Activated venv"
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[@]}" )" && pwd )" # find out script folder, when being sourced.
+echo "Script Folder: $SCRIPT_DIR"
+
+VENV_DIR="$SCRIPT_DIR/venv/bin/activate"
+echo "Activated venv in $VENV_DIR"
+source "$VENV_DIR"
+
 export PYTHONPATH="$SCRIPT_DIR/src/main/python"
 echo "Exported PYTHONPATH=$PYTHONPATH"
