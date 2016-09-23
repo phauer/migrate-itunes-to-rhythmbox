69a19d9d95fc60e8e28347b493b450e982fd4048
Philipp Hauer <info@philipphauer.de>
2016-09-23 18:31:59 +0200

    remove escaped copyright symbol from example data. different escape sequence won't be fixed.

---
+++ adhoc-scripts/compare-files.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+meld "../src/unittest/resources/expected_output/rhythmdb-count-ratings.xml" "../target/testoutput/rhythmdb-cout-ratings.xml"
--- adhoc-scripts/copy-output-to-rhythmbox.sh
+++ adhoc-scripts/copy-output-to-rhythmbox.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rhythmboxDir="~/.local/share/rhythmbox/"
 cp "$rhythmboxDir""playlists.xml" "$rhythmboxDir""playlists.xml.bak"
-cp target/testoutput/rhythmbox-playlists.xml "$rhythmboxDir""playlists.xml"
+cp "../target/testoutput/rhythmbox-playlists.xml" "$rhythmboxDir""playlists.xml"
